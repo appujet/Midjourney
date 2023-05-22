f8dc8f0911628a1e657ff1de3819ae971fb03013
Blacky <77108939+brblacky@users.noreply.github.com>
2023-05-22 21:12:46 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
-    token: "MTEwOTgzODg4MjgwNTEyNTE5MA.GUFZZe.2dAm9PEax-cdGPyL-iTsbEITXQmmU3lAHVIbMg",
+    token: "",
     clientId: "1109838882805125190",
     color: "#00ff00",
-    replicateToken: "r8_dhcgRHHhNvcyXFNhMKSQAMME8zZyzDC4DbylE",
+    replicateToken: "",
     model: "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf" as any
 }
